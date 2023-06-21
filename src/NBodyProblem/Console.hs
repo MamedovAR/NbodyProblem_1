d4fcea61cfeefdc1dce4b26a55612434174403e7
MamedovAR <a.mamedov1@g.nsu.ru>
2023-06-21 14:12:53 +0700

    New commit

--- src/NBodyProblem/Console.hs
+++ src/NBodyProblem/Console.hs
@@ -1,6 +1,17 @@
 import NBodyProblem.BurnesAndHut
 import System.IO.Unsafe
 
+showList :: [[Float]] -> IO ()
+showList lst = do
+	let lst' = map (\a -> (a!!0)*20 + (a!!1)) $ map (map (\a -> round $ a*20)) $ take 20 lst
+	let output = [if elem a lst' then '.' else ' ' | a <- [0..399]]
+	let showLine ln i = do
+		if i==-20 then return () else
+			putStrLn $ take 20 $ drop i
+			return $ unsafePerformIO $ showLine ln (i-20)
+	showLine output 380
+	return ()
+
 createList :: [IORef Node] -> [[Float]] -> IO [[Float]]
 createList bodies lst = do
 	if bodies == [] then return lst else
@@ -14,4 +25,5 @@ mainFunc bodies theta g dt max_iter sps = do
 		let bodies' = map (\b -> unsafePerformIO $ resetTo0thQuadrant b >> add b root) bodies
 		verlet bodies root theta g dt
 		lst <- createList bodies []
+		showList lst 380
 		return $ unsafePerformIO $ mainFunc bodies theta g dt (max_iter) (sps++[lst])
